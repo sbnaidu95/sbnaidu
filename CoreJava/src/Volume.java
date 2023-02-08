eeb9e3b779e51f5db010c96f4b54fcad417a7cd6
sbnaidu95 <bangarunaidu.s@gmail.com>
2023-02-08 11:46:52 +0530

    programs

--- CoreJava/src/Volume.java
+++ CoreJava/src/Volume.java
@@ -1,7 +1,8 @@
+import java.lang.Math;
 public class Volume {
     public static void main(String[]args ){
-        int l=10;
-        int result =l*l*l;
+        int l=15;
+        double result = Math.pow(l,3);
         System.out.println("volume of cube:"+result);
     }
 }
